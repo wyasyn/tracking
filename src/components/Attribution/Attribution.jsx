d2b36ea0ec729ce2d8324cb56c62e46bf37c2ad0
Yasin Walum <63132133+wyasyn@users.noreply.github.com>
2023-08-01 02:24:49 +0300

    Update Attribution.jsx
    link

--- src/components/Attribution/Attribution.jsx
+++ src/components/Attribution/Attribution.jsx
@@ -5,7 +5,7 @@ const Attribution = () => {
     <div>
          <p class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'>Frontend Mentor</a>. 
-        Coded by <a href="https://walumyas.netlify.app" target='_blank' rel='noreferrer'>Yasin Walum</a>.
+        Coded by <a href="https://ywalum.com" target='_blank' rel='noreferrer'>Yasin Walum</a>.
         </p>
     </div>
   )
